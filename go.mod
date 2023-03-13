bbf953f0e969166e7b383516a42ebe699e68147d
JS <jingshuai@petalmail.com>
2023-03-13 13:15:19 +0800

    引用github的logf

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_study
 
 go 1.20
+
+require github.com/Js179/logf v1.0.0
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"github.com/Js179/logf"
+	"time"
+)
 
+func main() {
+	logf.Info("start...")
+	logf.Infof("start... %v", time.Now().Format(time.DateTime))
+	logf.Error(errors.New("END"))
 }
